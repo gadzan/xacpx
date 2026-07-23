5c5e4959e5c881dfc039dde5d32bc2d719ef16f0
Gadzan Mak <gadzan@qq.com>
2026-07-23 19:22:34 +0800

    release: xacpx 0.17.0-beta.12

--- tests/unit/packages/package-metadata.test.ts
+++ tests/unit/packages/package-metadata.test.ts
@@ -16,10 +16,10 @@ test("root package publishes as xacpx and exposes plugin-api", () => {
   });
 });
 
-test("root package version is 0.17.0-beta.11", () => {
+test("root package version is 0.17.0-beta.12", () => {
   const pkg = readJson("package.json");
 
-  expect(pkg.version).toBe("0.17.0-beta.11");
+  expect(pkg.version).toBe("0.17.0-beta.12");
 });
 
 test("first-party channel plugins peer depend on xacpx", () => {
