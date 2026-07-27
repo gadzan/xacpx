5e29f9b3de505b95f36efad3d0db145bca79545d
gadzan <gadzan@qq.com>
2026-07-27 14:10:55 +0800

    release: 0.17.1

--- tests/unit/packages/package-metadata.test.ts
+++ tests/unit/packages/package-metadata.test.ts
@@ -16,10 +16,10 @@ test("root package publishes as xacpx and exposes plugin-api", () => {
   });
 });
 
-test("root package version is 0.17.0", () => {
+test("root package version is 0.17.1", () => {
   const pkg = readJson("package.json");
 
-  expect(pkg.version).toBe("0.17.0");
+  expect(pkg.version).toBe("0.17.1");
 });
 
 test("first-party channel plugins peer depend on xacpx", () => {
